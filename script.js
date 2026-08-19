71cdce54312dbdd35aefba021ff9e04c1c42b8b1
Marzi <116076643+marziezare@users.noreply.github.com>
2026-08-19 17:18:41 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',function(){
+setTimeout(function(){
+document.querySelector('.container
+).classList.add('loaded')
+},100)
+});
+function login(){
+alert("smile and arrive:)")
+window.location.href="pizza.html"
+}
+function orderrecipe(){
+alert("order seccesfull")
+window.location.href="pizza.html"}
+ function register(){
+alert("thank you for login")
+window.location.href="pizza.html"
+ }
